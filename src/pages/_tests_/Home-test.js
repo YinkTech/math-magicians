bb1f6a8a3d10791ca2ecbb6eb4cf5c1194e27cd4
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-10-13 13:44:02 +0100

    first test

---
+++ src/pages/_tests_/Home-test.js
@@ -0,0 +1,10 @@
+import Home from "../Home";
+
+describe("Home page", () => {
+    it("passing test", () => {
+        expect(true).toBeTruthy();
+    });
+    it("failing test", () => {
+        expect(false).toBeFalsy();
+    })
+});
