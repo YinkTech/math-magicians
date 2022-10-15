f84fd4e9cd97eb54b5cbf93f8c3acc55ecd44773
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-10-15 15:36:11 +0100

    full test

--- src/pages/_tests_/App.test.js
+++ src/pages/_tests_/App.test.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import App from './../../components/App';
-import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from "react-test-renderer";
 
-describe('App function test', () => {
-    test('displays the number clicked', () => {
-        render(<App />);
-
-        const twoBtn = screen.getByText('2');
-        const resultPara = screen.getByRole('none');
-        fireEvent.click(twoBtn);
-
-        expect(resultPara.textContent).toBe('2');
-    });
-
-    test('displays operation clicked', () => {
-        render(<App />);
-
-        const plusBtn = screen.getByText3('+');
-        const resultPara = screen.getByRole('none');
-        fireEvent.click(plusBtn);
-
-        expect(resultPara.textContent).toBe('0+');
-    });
-
-    test('displays correct summation', () => {
-        render(<App />);
-        const twoBtn = screen.getByText('2');
-        const threeBtn = screen.getByText('3');
-        const plusBtn = screen.getByText('+');
-        const equalBtn = screen.getByText('=');
-
-        const resultPara = screen.getByRole('none');
-
-        fireEvent.click(twoBtn);
-        fireEvent.click(plusBtn);
-        fireEvent.click(threeBtn);
-        fireEvent.click(equalBtn);
-
-        expect(resultPara.textContent).toBe('5');
-    });
+it("If it renders correctly", () => {
+    const app = renderer
+        .create(<App />)
+        .toJSON();
+        expect(app).toMatchSnapshot();
 });
